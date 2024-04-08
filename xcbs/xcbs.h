b34c6799ac744f185d7a3803973d24ae91c5efac
Artem Leshchenko <art.leshch@gmail.com>
2024-04-08 12:04:24 +0300

    Modify xcbs.h, now checks what OS is being used

--- xcbs/xcbs.h
+++ xcbs/xcbs.h
@@ -1,9 +1,9 @@
-﻿// xcbs.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#ifndef XCBS_H
+#define XCBS_H
 
 #include <stdio.h>
+
+#ifdef _WIN32
 
 #include <windows.h>
 #include <bluetoothapis.h>
@@ -11,4 +11,10 @@
 #include <devguid.h>
 #include <setupapi.h>
 
-// TODO: Reference additional headers your program requires here.
+#elif __linux__
+
+// probably something else...
+
+#endif
+
+#endif // XCBS_H
