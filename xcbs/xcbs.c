0dc4fe13b86e0e306a37f1eaea3ad6abe797a7af
Artem Leshchenko <art.leshch@gmail.com>
2024-04-08 11:07:08 +0300

    Add project files.

---
+++ xcbs/xcbs.c
@@ -0,0 +1,10 @@
+﻿#include "xcbs.h"
+
+int main() {
+	printf("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA...\n");
+	Sleep(1000);
+	printf("hahaha.\n");
+	Sleep(1000);
+	printf("i have to redo all my work from scratch.\n");
+	return 0;
+}
---
+++ xcbs/xcbs.h
@@ -0,0 +1,14 @@
+﻿// xcbs.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdio.h>
+
+#include <windows.h>
+#include <bluetoothapis.h>
+#include <bluetoothleapis.h>
+#include <devguid.h>
+#include <setupapi.h>
+
+// TODO: Reference additional headers your program requires here.
